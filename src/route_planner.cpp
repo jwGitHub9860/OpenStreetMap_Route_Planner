425e9abfaab9885d4f9b748724275323e78fa4a6
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2024-05-03 16:33:55 -0400

    TODO Corrections

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -25,8 +25,11 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
 
 float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
     //return abs(&start_node[0] - &end_node[0]) + abs(&start_node[1] - &end_node[1]);     // Manhattan Distance (h) = | x 1 − x 2 | + | y 1 − y 2 |
+    //return sqrt(pow((&node[0] - &end_node[0]), 2) + pow((&node[1] - &end_node[1]), 2));     // Distance Formula (h) = sqrt( ( x_2 − x_1 )^2 + ( y_2 − y_1 )^2 )
 
-    return sqrt(pow((&node[0] - &end_node[0]), 2) + pow((&node[1] - &end_node[1]), 2));     // Distance Formula (h) = sqrt( ( x_2 − x_1 )^2 + ( y_2 − y_1 )^2 )
+    return (*node).distance(*end_node);     // ACCESSES Distance Formula (h) = sqrt( ( x_2 − x_1 )^2 + ( y_2 − y_1 )^2 )
+    //return node->distance(*end_node);     // ACCESSES Distance Formula (h) = sqrt( ( x_2 − x_1 )^2 + ( y_2 − y_1 )^2 )
+    //return node->distance(end_node);     // ACCESSES Distance Formula (h) = sqrt( ( x_2 − x_1 )^2 + ( y_2 − y_1 )^2 )
 }
 
 
@@ -67,9 +70,7 @@ void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
 
 bool Compare(RouteModel::Node* node_1, RouteModel::Node* node_2)    // compares f-values of two nodes
 {
-    float f_1 = node_1->g_value + node_1->h_value;   // f_1 = g_1 + h_1
-    float f_2 = node_2->g_value + node_2->h_value;   // f_1 = g_2 + h_2
-    return f_1 > f_2;
+    return (node_1->g_value + node_1->h_value) > (node_2->g_value + node_2->h_value);   // return f_1 > f_2             f_1 = g_1 + h_1             f_2 = g_2 + h_2
 }
 
 RouteModel::Node *RoutePlanner::NextNode() {
@@ -77,7 +78,7 @@ RouteModel::Node *RoutePlanner::NextNode() {
 
     RouteModel::Node* low_sum_node = &open_list.back();  // WANT LAST ELEMENT (pointer to node in list with LOWEST SUM)          'end()' - returns POINTER          'back()' - returns LAST ELEMENT
 
-    open_list.pop_back(&low_sum_node);      // DON'T PUT ANYTHING IN () OF pop_back() since it ALWAYS deletes the LAST element of the list
+    open_list.pop_back();      // DON'T PUT ANYTHING IN () OF pop_back() since it ALWAYS deletes the LAST element of the list
 
     return low_sum_node;
 }
@@ -97,6 +98,17 @@ std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node 
     std::vector<RouteModel::Node> path_found;
 
     // TODO: Implement your solution here.
+    //auto final_node = *current_node + i;     // NOTE (delete later)
+    while (current_node != start_node)  // iterate through nodes until start_node
+    {
+        path_found.push_back(*current_node);
+        current_node = (*current_node).parent;  // changes "current_node" to "parent_node"
+        //current_node->distance();
+
+        distance += (*current_node).distance(*((*current_node).parent))  // adds distance from node to parent to "distance"
+    }
+    
+    reverse(open_list.begin(), open_list.end());    // reverse vector
 
     distance *= m_Model.MetricScale(); // Multiply the distance by the scale of the map to get meters.
     return path_found;
@@ -112,11 +124,34 @@ std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node 
 // - Store the final path in the m_Model.path attribute before the method exits. This path will then be displayed on the map tile.
 
 void RoutePlanner::AStarSearch() {
-    RouteModel::Node *current_node = nullptr;
+    RouteModel::Node *current_node = nullptr;   // initializes *current_node
 
     // TODO: Implement your solution here.
-    AddNeighbors(*current_node);
-    NextNode();
-    auto final_path = ConstructFinalPath(*current_node);
-    m_Model.path(final_path);
+    open_list{};    // HOLDS ALL NODES (ALREADY HAS NODES IN IT) intialize?
+    //vector<vector<int>> open_nodes_list{};  // JUST IN CASE
+
+    // starting node    JUST IN CASE (maybe)
+    /*int x = start_x;
+    int y = start_y;
+    int g = 0;
+    int h = CalculateHValue(*current_node);   // JUST IN CASE (maybe)*/
+    
+    AddNeighbors(current_node);    // CHECKS IF GOAL WAS REACHED (maybe?)
+
+    while (open_list.size() > 0)   // checks if open_list vector is NOT EMPTY
+    {
+        RouteModel::Node* next_node = NextNode();     // sorts "open_list{}" & returns next node
+
+        RouteModel::Node* x = &next_node[0];    // obtains x value from "next_node"
+        RouteModel::Node* y = &next_node[1];    // obtains y value from "next_node"
+
+        if (x == &end_node[0] && y == &end_node[1])  // CHECKS IF GOAL WAS REACHED
+        {
+            m_Model.path = ConstructFinalPath(current_node);    // stores final path in m_Model.path
+            return ConstructFinalPath(end_node);   // returns FINAL PATH & Exits while loop
+        }
+        
+    }
+    
+    AddNeighbors(current_node);    // ALSO checks other nodes (maybe?)
 }
