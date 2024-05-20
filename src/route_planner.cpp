89894ac6393a1c1f65551cb0b7a556f41e286ad8
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2024-05-20 18:28:20 -0400

    Added comments about 'emplace_back()'

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -46,7 +46,7 @@ void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
         neighbor->g_value = current_node->g_value + current_node->distance(*neighbor);      // sets g_value         g_2 = g + 1
         neighbor->h_value = CalculateHValue(neighbor);      // sets h_value
 
-        open_list.emplace_back(current_node);      // ADDS "current_node" to "open_list"
+        open_list.emplace_back(current_node);      // ADDS "current_node" to "open_list"        'emplace_back()' adds node DIRECTLY into container
         neighbor->visited = true;      // MARKS node as visited (true)
     }
 }
@@ -91,7 +91,7 @@ std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node 
     // TODO: Implement your solution here.
     while (current_node != start_node)  // iterate through nodes until start_node
     {
-        path_found.emplace_back(*current_node);
+        path_found.emplace_back(*current_node);        // 'emplace_back()' adds node DIRECTLY into container
 
         distance += (*current_node).distance(*((*current_node).parent))  // adds distance from node to parent to "distance"
         current_node = (*current_node).parent;  // changes "current_node" to "parent_node"
